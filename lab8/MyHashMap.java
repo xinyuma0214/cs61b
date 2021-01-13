0fa2bf040b27201fcd11d6cc61bde41bea117954
xinyuma0214 <xinyuma@umich.edu>
2021-01-13 18:33:45 -0500

    lab8 is done

--- lab8/MyHashMap.java
+++ lab8/MyHashMap.java
@@ -4,7 +4,21 @@ import java.util.Iterator;
 import java.util.Set;
 
 public class MyHashMap<K,V> implements Map61B<K,V>{
+    private int numofBucket;
+    private int numofEntries = 0;
+    private double num;
+    private HashSet<K> keys;
+    private ArrayList<ArrayList<Entry>> buckets;
 
+    private class Entry{
+        private K key;
+        private V value;
+
+        public Entry(K key, V value){
+            this.key = key;
+            this.value = value;
+        }
+    }
     public MyHashMap(){
         this(16);
     }
@@ -13,56 +27,102 @@ public class MyHashMap<K,V> implements Map61B<K,V>{
     }
     public MyHashMap(int initialSize, double loadFactor){
         keys = new HashSet<>();
+        buckets = new ArrayList<>();
         numofBucket = initialSize;
+        num = loadFactor;
         for(int i = 0; i < numofBucket; i++){
-
+            buckets.add(new ArrayList<Entry>());
         }
     }
-    private int numofBucket;
-    private HashSet<K> keys;
-    private ArrayList<>
+
+
     @Override
     public void clear() {
-
+        keys = new HashSet<>();
+        buckets = new ArrayList<>();
+        numofBucket = 0;
+        numofEntries = 0;
     }
-
+    public void resize(int capacity){
+        ArrayList<ArrayList<Entry>> newBuckets = new ArrayList<>();
+        for(int i = 0; i < capacity; i++){
+            newBuckets.add(new ArrayList<>());
+        }
+        for(K key : this){
+            int index = hash(key,capacity);
+            newBuckets.get(index).add(new Entry(key,get(key)));
+            this.numofBucket = capacity;
+            this.buckets = newBuckets;
+        }
+    }
     @Override
     public boolean containsKey(K key) {
-        return false;
+        return keys.contains(key);
     }
 
     @Override
     public V get(K key) {
+        if(!containsKey(key)) {
+            return null;
+        }
+        int index = hash(key,numofBucket);
+        for(Entry entry : buckets.get(index)){
+            if(entry.key.equals(key)){
+                return entry.value;
+            }
+        }
         return null;
     }
+    public int hash(K key, int capacity){
+        return Math.floorMod(key.hashCode(),capacity);
 
+    }
+    public void update(K key, V value){
+        int index = hash(key,numofBucket);
+        for(Entry entry : buckets.get(index)){
+            if(entry.key.equals(key)){
+                entry.value = value;
+            }
+        }
+    }
     @Override
     public int size() {
-        return 0;
+        return numofEntries;
     }
 
     @Override
     public void put(K key, V value) {
+        if(containsKey(key)){
+            update(key,value);
+            return;
+        }
+        if(size() >= numofBucket * num){
+            resize(numofBucket * 2);
+        }
+        int index = hash(key,numofBucket);
+        buckets.get(index).add(new Entry(key,value));
+        keys.add(key);
+        numofEntries += 1;
 
     }
 
     @Override
     public Set<K> keySet() {
-        return null;
+        return keys;
     }
 
     @Override
     public V remove(K key) {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public V remove(K key, V value) {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public Iterator<K> iterator() {
-        return null;
+        return keys.iterator();
     }
 }
